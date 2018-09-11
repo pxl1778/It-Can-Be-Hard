213d937507d9a6fc987a8c92f43d22c7b0bf5833
pxl1778 <pxl1778@rit.edu>
2018-09-10 20:41:46 -0400

    phone scene begin

---
+++ Lighthouse/Assets/Scripts/UI Scripts/TextingManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextingManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void clickReply()
+    {
+        //set to button in phone
+        //start type out message
+        //create new dialogue box after finishing
+        //make parent to dialogue box the same as the scrollview
+        //scroll scrollview down
+    }
+}
